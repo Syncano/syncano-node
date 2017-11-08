f4a1f4a3aa279bb1844d7bfffcd20594ecd90625
mkucharz <m@kucharz.net>
2017-11-08 18:41:39 +0100

    fix(test): missing files

---
+++ packages/lib-js-core/test/e2e/endpoint.js
@@ -0,0 +1,27 @@
+import {getRandomString, createTestInstance, deleteTestInstance} from '../utils'
+
+describe('Endpoint', function () {
+  const instanceName = getRandomString()
+
+  before(function (done) {
+    createTestInstance(instanceName)
+      .then(instanceObj => {
+        done()
+      })
+      .catch(err => {
+        console.log(err)
+        err.response.text().then(text => {
+          console.log(text)
+          done(err)
+        })
+      })
+  })
+
+  after(function (done) {
+    deleteTestInstance(instanceName).then(() => {
+      done()
+    })
+  })
+
+  // TODO: add tests!
+})
