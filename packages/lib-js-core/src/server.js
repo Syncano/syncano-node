ccf749f0ac5b1b625c4e9fdc22f2020f0e431b5e
Dennor <matysiewicz.kamil@gmail.com>
2017-12-12 14:25:00 +0100

    Make users property in server to be a proxy. (#58)

--- packages/lib-js-core/src/server.js
+++ packages/lib-js-core/src/server.js
@@ -16,7 +16,6 @@ const server = (ctx = {}) => {
   const config = getConfig()
 
   const _class = new Class(config)
-  const users = new Users(config)
   const event = new Event(config)
   const channel = new Channel(config)
   const socket = new Socket(config)
@@ -27,7 +26,14 @@ const server = (ctx = {}) => {
 
   return {
     _class,
-    users,
+    users: new Proxy(
+      {},
+      {
+        get(target, prop) {
+          return new Users(config)[prop];
+        }
+      }
+    ),
     account,
     instance,
     event,
