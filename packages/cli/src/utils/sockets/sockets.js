0cc0ced72110d5c6318aebeb01892dbf8df5e85b
Adam Wardecki <adam.wardecki@syncano.com>
2018-03-22 15:25:16 +0100

    chore(cli): fix linter errors

--- packages/cli/src/utils/sockets/sockets.js
+++ packages/cli/src/utils/sockets/sockets.js
@@ -1032,7 +1032,7 @@ class Socket {
               // Build registry socket.
               try {
                 await this.build()
-              } catch(e) {
+              } catch (e) {
                 return reject(e)
               }
               return resolve()
