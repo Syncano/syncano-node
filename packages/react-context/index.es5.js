28a27a7f9adcdb8dd6cbef1c935a9f168a416d72
Kasper Mikiewicz <IderedPL@gmail.com>
2018-04-19 06:42:38 +0200

    fix(react-context): add react import (#221)

--- packages/react-context/index.es5.js
+++ packages/react-context/index.es5.js
@@ -1,13 +1,21 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.SyncanoContext = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 exports.withSyncano = withSyncano;
-var SyncanoContext = exports.SyncanoContext = React.createContext();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const SyncanoContext = exports.SyncanoContext = _react2.default.createContext();
 
 // This function takes a component...
 function withSyncano(Component) {
@@ -15,12 +23,10 @@ function withSyncano(Component) {
   return function SyncanoComponent(props) {
     // ... and renders the wrapped component with the context theme!
     // Notice that we pass through any additional props as well
-    return React.createElement(
+    return _react2.default.createElement(
       SyncanoContext.Consumer,
       null,
-      function (syncano) {
-        return React.createElement(Component, _extends({}, props, { syncano: syncano }));
-      }
+      syncano => _react2.default.createElement(Component, _extends({}, props, { syncano: syncano }))
     );
   };
 }
--- packages/react-context/index.js
+++ packages/react-context/index.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const SyncanoContext = React.createContext()
 
 // This function takes a component...
