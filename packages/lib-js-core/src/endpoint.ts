ace30a5303883fa13eedd08bc9846d4454f7c7d6
Kasper Mikiewicz <IderedPL@gmail.com>
2018-04-23 08:11:53 +0200

    feat(lib-js-core): add endpoint type definitions

--- packages/lib-js-core/src/endpoint.ts
+++ packages/lib-js-core/src/endpoint.ts
@@ -1,42 +1,75 @@
+import {RequestInit} from 'node-fetch'
 import QueryBuilder from './query-builder'
 
-/**
- * Running endpoints.
- */
 export default class Endpoint extends QueryBuilder {
+  /**
+   * Send POST request to Syncano Endpoiint
+   *
+   * @param endpoint Endpoint path in format `socket-name/endpoint-name`. For example: `user-auth/register`.
+   * @param [data={}] Data passed to endpoint.
+   * @param [options={}] node-fetch options
+   */
   public post (endpoint: string, body = {}, options = {}) {
     const fetch = this.fetch.bind(this)
 
-    return fetch(this._url(endpoint), {
-      body: this._parseBody(body),
+    return fetch(this.url(endpoint), {
+      body: this.parseBody(body),
       method: 'POST',
       ...options
     })
   }
 
-  public get (endpoint: string, data = {}, options = {}) {
+  /**
+   * Send GET request to Syncano Endpoiint
+   *
+   * @param endpoint Endpoint path in format `socket-name/endpoint-name`. For example: `user-auth/register`.
+   * @param [data={}] Data passed to endpoint.
+   * @param [options={}] node-fetch options
+   */
+  public get (endpoint: string, data: any = {}, options: RequestInit = {}) {
     return this.post(endpoint, {...data, _method: 'GET'}, options)
   }
 
-  public delete (endpoint: string, data = {}, options = {}) {
+  /**
+   * Send DELETE request to Syncano Endpoiint
+   *
+   * @param endpoint Endpoint path in format `socket-name/endpoint-name`. For example: `user-auth/register`.
+   * @param [data={}] Data passed to endpoint.
+   * @param [options={}] node-fetch options
+   */
+  public delete (endpoint: string, data: any = {}, options: RequestInit = {}) {
     return this.post(endpoint, {...data, _method: 'DELETE'}, options)
   }
 
-  public put (endpoint: string, data = {}, options = {}) {
+  /**
+   * Send PUT request to Syncano Endpoiint
+   *
+   * @param endpoint Endpoint path in format `socket-name/endpoint-name`. For example: `user-auth/register`.
+   * @param [data={}] Data passed to endpoint.
+   * @param [options={}] node-fetch options
+   */
+  public put (endpoint: string, data: any = {}, options: RequestInit = {}) {
     return this.post(endpoint, {...data, _method: 'PUT'}, options)
   }
 
-  public patch (endpoint: string, data = {}, options = {}) {
+  /**
+   * Send PATCH request to Syncano Endpoiint
+   *
+   * @param endpoint Endpoint path in format `socket-name/endpoint-name`. For example: `user-auth/register`.
+   * @param [data={}] Data passed to endpoint.
+   * @param [options={}] node-fetch options
+   */
+  public patch (endpoint: string, data: any = {}, options: RequestInit = {}) {
     return this.post(endpoint, {...data, _method: 'PATCH'}, options)
   }
 
-  private _url (endpoint: string) {
+  private url (endpoint: string) {
     const {instanceName, spaceHost} = this.instance
 
     return `https://${instanceName}.${spaceHost}/${endpoint}/`
   }
 
-  private _parseBody (body: any) {
+  private parseBody (body: any) {
     const isBodyAnObject = typeof body === 'object'
 
     return isBodyAnObject ? JSON.stringify({...body}) : body
