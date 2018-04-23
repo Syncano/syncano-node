dfdacaf05a7be3b715aa521b107491808a573757
Kasper Mikiewicz <IderedPL@gmail.com>
2018-04-23 09:40:30 +0200

    feat(lib-js-core): add socket type definitions

--- packages/lib-js-core/src/socket.ts
+++ packages/lib-js-core/src/socket.ts
@@ -1,10 +1,63 @@
 import QueryBuilder from './query-builder'
 
+export interface Socket {
+  name: string
+  description: string
+  created_at: string
+  updated_at: string
+  version: string
+  status: string
+  status_info: any
+  install_url: any
+  metadata: {
+    name: string
+    keywords: string[]
+    sources: {
+      [x: string]: string
+    }
+  }
+  config: object,
+  installed: {
+    endpoints: {
+      [endpointName: string]: {
+        script: string
+        runtime: string
+      }
+    }
+    classes: {
+      [className: string]: {
+        [fieldName: string]: string
+      }
+    }
+  }
+  files: {
+    [fileName: string]: {
+        checksum: string
+        size: number
+        file: string
+        helper: boolean
+    }
+  }
+  environment: string,
+  links: {
+    self: string
+    update: string
+    endpoints: string
+    handlers: string
+    zip_file: string
+  }
+}
+
 /**
  * Connection between Sockets.
  */
-export default class Socket extends QueryBuilder {
-  public get (socketName: string) {
+export default class SocketClass extends QueryBuilder {
+  /**
+   * Get Syncano Socket configuration.
+   *
+   * @param socketName Name of socket to fetch
+   */
+  public get (socketName: string): Promise<Socket> {
     return new Promise((resolve, reject) => {
       const headers = {
         'X-API-KEY': this.instance.accountKey
@@ -15,7 +68,12 @@ export default class Socket extends QueryBuilder {
     })
   }
 
-  public delete (socketName: string) {
+  /**
+   * Delete Syncano Socket
+   *
+   * @param socketName Name of socket to delete
+   */
+  public delete (socketName: string): Promise<void> {
     return new Promise((resolve, reject) => {
       const headers = {
         'X-API-KEY': this.instance.accountKey
@@ -29,7 +87,14 @@ export default class Socket extends QueryBuilder {
     })
   }
 
-  public list () {
+  /**
+   * Get list of Syncano Sockets in current instance
+   */
+  public list (): Promise<{
+    next: string|null
+    prev: string|null
+    objects: Socket[]
+  }> {
     return new Promise((resolve, reject) => {
       const headers = {
         'X-API-KEY': this.instance.accountKey
