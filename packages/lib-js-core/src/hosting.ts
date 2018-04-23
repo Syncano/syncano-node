97e151086baed4b93df189347167e05c021e122f
Kasper Mikiewicz <IderedPL@gmail.com>
2018-04-23 10:27:46 +0200

    feat(lib-js-core): add hosting type definitions

--- packages/lib-js-core/src/hosting.ts
+++ packages/lib-js-core/src/hosting.ts
@@ -3,47 +3,102 @@ import QueryBuilder from './query-builder'
 
 const debug = logger('core:hosting')
 
-/**
- * Hosting related operations.
- */
-export default class Hosting extends QueryBuilder {
-  public getFile (hostingId: string, fileId: string) {
+export interface File {
+  id: number
+  path: string
+  size: number
+  checksum: string
+  links: {
+    self: string
+    [x: string]: string
+  }
+}
+
+export interface Hosting {
+  name: string
+  is_default: boolean
+  description: string
+  created_at: string
+  updated_at: string
+  domains: string[]
+  is_active: boolean
+  ssl_status: string
+  auth: object
+  config: {
+    browser_router: boolean
+  }
+  links: {
+    self: string
+    files: string
+    set_default: string
+    enable_ssl: string
+    [x: string]: string
+  }
+}
+
+export default class HostingClass extends QueryBuilder {
+  /**
+   * Get single hosting file.
+   *
+   * @param hostingName Name of hosting
+   * @param fileId Id of file
+   */
+  public getFile (hostingName: string, fileId: string): Promise<File> {
     debug('getFile')
     return new Promise((resolve, reject) => {
       const headers = {
         'X-API-KEY': this.instance.accountKey
       }
-      this.fetch(this.urlFiles(hostingId, fileId), {}, headers)
+      this.fetch(this.urlFiles(hostingName, fileId), {}, headers)
         .then(resolve)
         .catch(reject)
     })
   }
 
-  public listFiles (hostingId: string) {
+  /**
+   * Get list of files in given hosting
+   * @param hostingName Name of hosting
+   */
+  public listFiles (hostingName: string): Promise<{
+    next: string|null
+    prev: string|null
+    objects: File[]
+  }> {
     debug('listFiles')
     return new Promise((resolve, reject) => {
       const headers = {
         'X-API-KEY': this.instance.accountKey
       }
-      this.fetch(this.urlFiles(hostingId), {}, headers)
+      this.fetch(this.urlFiles(hostingName), {}, headers)
         .then((res) => resolve(res.objects))
         .catch(reject)
     })
   }
 
-  public get (hostingId: string) {
+  /**
+   * Get single hosting configuration
+   * @param hostingName Name of hosting
+   */
+  public get (hostingName: string): Promise<Hosting> {
     debug('get')
     return new Promise((resolve, reject) => {
       const headers = {
         'X-API-KEY': this.instance.accountKey
       }
-      this.fetch(this.url(hostingId), {}, headers)
+      this.fetch(this.url(hostingName), {}, headers)
         .then(resolve)
         .catch(reject)
     })
   }
 
-  public updateFile (hostingId: string, fileId: string, payload: any) {
+  /**
+   * Update hosting file
+   *
+   * @param hostingName Name of hosting
+   * @param fileId Id of hosting
+   * @param payload File payload
+   */
+  public updateFile (hostingName: string, fileId: string, payload: any): Promise<File> {
     debug('updateFile')
     return new Promise((resolve, reject) => {
       const headers = payload.getHeaders()
@@ -54,13 +109,20 @@ export default class Hosting extends QueryBuilder {
         method: 'PATCH'
       }
 
-      this.fetch(this.urlFiles(hostingId, fileId), options, headers)
+      this.fetch(this.urlFiles(hostingName, fileId), options, headers)
         .then(resolve)
         .catch(reject)
     })
   }
 
-  public deleteFile (hostingId: string, fileId: string) {
+  /**
+   * Delete hosting file
+   *
+   * @param hostingName Name of hosting
+   * @param fileId Id of hosting
+   * @param payload File payload
+   */
+  public deleteFile (hostingName: string, fileId: string): Promise<void> {
     debug('deleteFile')
     return new Promise((resolve, reject) => {
       const headers = {
@@ -68,13 +130,20 @@ export default class Hosting extends QueryBuilder {
       }
       const options = { method: 'DELETE' }
 
-      this.fetch(this.urlFiles(hostingId, fileId), options, headers)
+      this.fetch(this.urlFiles(hostingName, fileId), options, headers)
         .then(resolve)
         .catch(reject)
     })
   }
 
-  public uploadFile (hostingId: string, payload: any) {
+  /**
+   * Upload new file to hosting
+   *
+   * @param hostingName Name of hosting
+   * @param fileId Id of hosting
+   * @param payload File payload
+   */
+  public uploadFile (hostingName: string, payload: any): Promise<File> {
     debug('uploadFile')
     return new Promise((resolve, reject) => {
       const headers = payload.getHeaders()
@@ -85,27 +154,27 @@ export default class Hosting extends QueryBuilder {
         method: 'POST'
       }
 
-      this.fetch(this.urlFiles(hostingId), options, headers)
+      this.fetch(this.urlFiles(hostingName), options, headers)
         .then(resolve)
         .catch(reject)
     })
   }
 
-  private urlFiles (hostingId: string, fileId?: string) {
+  private urlFiles (hostingName: string, fileId?: string) {
     const {instanceName} = this.instance
 
     if (fileId) {
-      return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/${hostingId}/files/${fileId}/`
+      return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/${hostingName}/files/${fileId}/`
     }
 
-    return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/${hostingId}/files/`
+    return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/${hostingName}/files/`
   }
 
-  private url (hostingId: string) {
+  private url (hostingName: string) {
     const {instanceName} = this.instance
 
-    if (hostingId) {
-      return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/${hostingId}/`
+    if (hostingName) {
+      return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/${hostingName}/`
     }
 
     return `${this.getSyncanoURL()}/instances/${instanceName}/hosting/`
