372673df5e421adff3f76920b72c712a6b01ed1b
Jan Rochalski <qwsazxxzaswq@wp.pl>
2019-03-04 11:38:16 +0100

    chore(): add type & change to new backup class

--- packages/cli/src/commands/backup/create.ts
+++ packages/cli/src/commands/backup/create.ts
@@ -1,3 +1,5 @@
+import {BackupClass} from '@syncano/core/backup'
+
 import Command from '../../base_command'
 import {SimpleSpinner} from '../../commands_helpers/spinner'
 
@@ -5,18 +7,18 @@ export default class BackupsCreate extends Command {
   static description = 'Create backup'
   static flags = {}
 
-  Backups: any
+  Backup: BackupClass
 
   async run() {
     await this.session.isAuthenticated() || this.exit(1)
     await this.session.hasProject() || this.exit(1)
 
-    this.Backups = this.session.connection.backups
+    this.Backup = this.session.connection.backup
 
     const spinner = new SimpleSpinner(this.p(2)('Creating backup...'))
     try {
       spinner.start()
-      const backup = await this.Backups.create()
+      const backup = await this.Backup.create()
       const status = await this._createBackup(backup.id)
       spinner.stop()
       spinner.succeed(this.p(2)(`Backup was created (${status.id}).`))
@@ -24,16 +26,18 @@ export default class BackupsCreate extends Command {
       this.error(err.message)
       this.exit(1)
     }
+
     this.exit(0)
   }
 
   async _createBackup(id) {
     await this.timeout(3000)
-    const backup = await this.Backups.get(id)
+    const backup = await this.Backup.get(id)
 
     if (backup.status !== 'success') {
       return this._createBackup(id)
     }
+
     return backup
   }
 
